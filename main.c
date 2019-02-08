53c34c233479e4869e6a081f55c5b5533dd49868
TarunAK <tarun.kalikivaya@hotmail.com>
2019-02-08 17:26:42 -0500

    compiled

---
+++ main.c
@@ -0,0 +1,78 @@
+#include "msp.h"
+
+
+void configNVIC(void)
+{	
+	NVIC_SetPriority(TA0_N_IRQn, 2);
+	NVIC_ClearPendingIRQ(TA0_N_IRQn);
+	NVIC_EnableIRQ(TA0_N_IRQn);
+	
+	NVIC_SetPriority(TA1_N_IRQn, 2);
+	NVIC_ClearPendingIRQ(TA1_N_IRQn);
+	NVIC_EnableIRQ(TA1_N_IRQn);
+}
+
+void configGlobalInterrupts(void)
+{
+   __ASM("CPSIE I");  
+}
+
+void configLED(void)
+{
+	P1DIR |= 0x01;
+	P2DIR |= 0x07;
+
+	P1DS &= ~0x01;
+	P2DS &= ~0x07;
+
+	P1OUT &= ~0x01;
+	P2OUT &= ~0x07;
+}
+
+void configTimer(void)
+{
+	TIMER_A0->CTL = MC_1 | ID_1 | TASSEL_1 | TAIE;
+	TIMER_A0->CTL &= ~TAIFG;
+	TIMER_A0->CCR[0] = 0xFFFF;
+	
+	TIMER_A1->CTL = MC_3 | ID_1 | TASSEL_1 | TAIE;
+	TIMER_A1->CTL &= ~TAIFG;
+	TIMER_A1->CCR[0] = 0xFFFF;
+	TIMER_A1->CCR[1] = 0xFFFF;
+}
+
+int main(void) 
+{
+	WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD; // Disable watchdog timer
+	
+	// Port 1 GPIO
+	P1SEL0 &= ~0x01;
+	P1SEL1 &= ~0x01;
+	
+	// Port 2 GPIO
+	P2SEL0 &= ~0x07;
+	P2SEL1 &= ~0x07;
+	
+	
+	configNVIC();
+	configGlobalInterrupts();
+	configLED();
+	configTimer();
+	
+   while(1)
+   {
+	   __ASM("WFI"); // Wait for interrupt, else sleep
+   }
+}
+
+void PORT1_IRQHandler(void)
+{
+	
+}
+
+void TA0_N_IRQHandler(void)
+{
+	
+	//TIMER_A0->CCTL0 &= ~TAIFG;
+	//NVIC_ClearPendingIRQ(TA0_N_IRQn); // Clear pending Timer A interrupt 
+}
