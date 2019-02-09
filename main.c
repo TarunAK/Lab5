8baa739e302636962f9fa7621f6184709cdd95a6
TarunAK <tarun.kalikivaya@hotmail.com>
2019-02-08 19:05:02 -0500

    third'

--- main.c
+++ main.c
@@ -1,11 +1,14 @@
 #include "msp.h"
 
-
 void configNVIC(void)
 {	
 	NVIC_SetPriority(TA0_N_IRQn, 2);
 	NVIC_ClearPendingIRQ(TA0_N_IRQn);
 	NVIC_EnableIRQ(TA0_N_IRQn);
+	
+	NVIC_SetPriority(TA0_0_IRQn, 2);
+	NVIC_ClearPendingIRQ(TA0_0_IRQn);
+	NVIC_EnableIRQ(TA0_0_IRQn);
 	
 	NVIC_SetPriority(TA1_N_IRQn, 2);
 	NVIC_ClearPendingIRQ(TA1_N_IRQn);
@@ -34,11 +37,16 @@ void configTimer(void)
 	TIMER_A0->CTL = MC_1 | ID_1 | TASSEL_1 | TAIE;
 	TIMER_A0->CTL &= ~TAIFG;
 	TIMER_A0->CCR[0] = 0xFFFF;
+	TIMER_A0->CCR[1] = 0x1988; 
+	
+	TIMER_A0->CTL = CCIE;
+	TIMER_A0->CCTL[0] &= ~CCIFG;
+	TIMER_A0->CCTL[0] &= ~CCIFG;
 	
 	TIMER_A1->CTL = MC_3 | ID_1 | TASSEL_1 | TAIE;
 	TIMER_A1->CTL &= ~TAIFG;
-	TIMER_A1->CCR[0] = 0xFFFF;
-	TIMER_A1->CCR[1] = 0xFFFF;
+	TIMER_A1->CCR[0] = 0x7FFF;
+	//TIMER_A1->CCR[1] = 0xFFFF;
 }
 
 int main(void) 
@@ -65,14 +73,17 @@ int main(void)
    }
 }
 
-void PORT1_IRQHandler(void)
+void TA0_0_IRQHandler(void)
 {
-	
 }
 
-void TA0_N_IRQHandler(void)
-{
-	
-	//TIMER_A0->CCTL0 &= ~TAIFG;
-	//NVIC_ClearPendingIRQ(TA0_N_IRQn); // Clear pending Timer A interrupt 
-}
+//void TA0_N_IRQHandler(void)
+//{
+//	if ((TIMER_A0->CTL & TAIFG) == 1)
+//	{
+//	}
+//	TA0CTL &= ~TAIFG;
+//	P1OUT ^= 0x01;
+//	//TIMER_A0->CCTL0 &= ~TAIFG;
+//	//NVIC_ClearPendingIRQ(TA0_N_IRQn); // Clear pending Timer A interrupt 
+//}
